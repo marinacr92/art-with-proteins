4f5eb8c7b74ba1a1fbde03a14576633320fa8220
marinacr92 <marinacaro92@gmail.com>
2023-05-13 13:54:48 +0200

    Add tablet version

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -4,10 +4,12 @@ import watercolor from '../images/watercolor.png';
 const Splash = () => {
   return (
     <>
-      <div className="splash">
-        <h1 className="splash-title">Art with Proteins</h1>
-        <img className="splash-watercolor" alt="" src={watercolor} />
-      </div>
+      <section className="splash">
+        <div className="splash-container">
+          <h1 className="splash-title">Art with Proteins</h1>
+          <img className="splash-watercolor" alt="" src={watercolor} />
+        </div>
+      </section>
     </>
   );
 };
